1ba7d403fe9178b6f9c018893732cc5a4d250231
luisppinto <72820660+luisppinto@users.noreply.github.com>
2020-11-25 18:19:26 +0000

    1

--- tp/tp1/v6/v6041.py
+++ tp/tp1/v6/v6041.py
@@ -11,7 +11,7 @@ cd=[] # Lista vazia (a preencher por get_cd()) de todos os cds e respetivos cont
 title=[]
 
 def get_codigo():
-	with open('v6051.py', encoding='utf-8') as original, open('codigo.txt', "w", encoding='utf-8') as target:
+	with open('v6041.py', encoding='utf-8') as original, open('codigo.txt', "w", encoding='utf-8') as target:
 		target.writelines(original.readlines())
 
 def get_cd():
@@ -32,12 +32,12 @@ def create_page(cd):
 	</head>
 	<body>
 	<div class="header">
-				<h1>Catálogo de CDs</h1>
+				<h1>Cátalogo de CDs</h1>
 		</div>
 		<div class="nav">
 			<table class="nav-tab">
 				<tr>
-						<th><a href="index.html"><div class="hed">Lista de Títulos</div></a></th>
+						<th><a href="index.html"><div class="hed">Lista de Titulos</div></a></th>
 						<th><a href="relatorio.html"><div class="hed">Relatório</div></a></th>
 						<th><a href="codigo.txt" target="_blank"><div class="hed">Código</a></div></th>
 				</tr>
@@ -104,12 +104,12 @@ def index(cd):
 		</head>
 		<body>
 		<div class="header">
-				<h1>Catálogo de CDs</h1>
+				<h1>Cátalogo de CDs</h1>
 		</div>
 		<div class="nav">
 			<table class="nav-tab">
 				<tr>
-						<th><a href="index.html"><div class="hed">Lista de Títulos</div></a></th>
+						<th><a href="index.html"><div class="hed">Lista de Titulos</div></a></th>
 						<th><a href="relatorio.html"><div class="hed">Relatório</div></a></th>
 						<th><a href="codigo.txt" target="_blank"><div class="hed">Código</a></div></th>
 				</tr>
@@ -148,13 +148,13 @@ def relatorio():
 		</head>
 		<body>
 		<div class="header">
-				<h1>Catálogo de CDs</h1>
+				<h1>Cátalogo de CDs</h1>
 		</div>
 
 		<div class="nav">
 			<table>
 				<tr>
-					<th><a href="index.html"><div class="hed">Lista de Títulos</div></a></th>
+					<th><a href="index.html"><div class="hed">Lista de Titulos</div></a></th>
 						<th><a href="relatorio.html"><div class="hed">Relatório</div></a></th>
 										<th><a href="codigo.txt" target="_blank"><div class="hed">Código</a></div></th>
 								</tr>
@@ -162,10 +162,10 @@ def relatorio():
 		</div>
 		<div class="main_content">
 			<h1>Relatório</h1>
-			<h2> Ficheiro catalogotp1.xml </h2>
+			<h2> Ficheiro Catálogotp1.xml </h2>
 			 <p class="conteudo_relatorio">
-				O ficheiro catalogotp1.xml contém um conjunto de dados relativos a CDs de Música.<br>
-				Estes CDs contêm um título, de carácter obrigatório, contém ainda um artista, uma capa de álbum, um país, uma produtora, uma descrição e um ano.<br>
+				O ficheiro Catálogotp1.xml contém um conjunto de dados relativos a CDs de Música.<br>
+				Estes CDs contém um titulo, de caracter obrigatório, contém ainda um artista, um país, uma produtora, uma descrição e um ano.<br>
 				Abaixo podemos ver um excerto de código em XML:
 			</p>
 			<p class="codigo">
@@ -173,18 +173,16 @@ def relatorio():
 				&nbsp&nbsp&nbsp  &ltCD&gt<br>
 				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp &lttitle&gtA Vida Que Eu Escolhi&lt/title&gt<br>
 				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp &ltartist&gtTony Carreira&lt/artist&gt<br>
-				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp &ltartwork&gthttps://img.discogs.com/ZrC2cjDjuh_mremQ6o5kXKlTFAQ=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-5263584-1389034439-9728.jpeg.jpg&lt/artwork&gt<br>
 				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp &ltcountry&gtPortugal&lt/country&gt<br>
 				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp &ltcompany&gtEspacial&lt/company&gt<br>
-				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp &ltdescription&gtA Vida Que Eu Escolhi é o décimo segundo álbum de estúdio a solo do cantor português Tony Carreira. Foi lançado em 2006 pela editora Espacial. Contém 15 faixas, das quais se destacam "Mesmo Que Seja Mentira" e "É Melhor (Dizer Adeus)" (anteriormente editadas como extras no álbum Ao Vivo no Coliseu, também de 2006), bem como "O Que Vai Ser De Mim (Quando Fores Embora)" e "A Vida Que Eu Escolhi", 4 temas que fazem parte da compilação "20 Anos de Canções", lançada em 2008. Este trabalho esteve, ao todo, 64 semanas, no Top Oficial da AFP, a tabela semanal dos 30 álbuns mais vendidos em Portugal. Entrou na época de Natal de 2006 directamente para a 4ª posição, atingindo o 1º lugar à quarta semana, lugar que disputaria com André Sardet e Madonna e que ocuparia por mais 3 ocasiões mas que perderia definitivamente para José Afonso. &lt/description&gt<br>
+				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp &ltdescription&gtA Vida Que Eu Escolhi é o décimo segundo álbum de estúdio a solo do cantor português Tony Carreira. Foi lançado em 2006 pela editora Espacial. &lt/description&gt<br>
 				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp &ltyear&gt2006&lt/year&gt &lt/CD&gt<br>
 				&nbsp&nbsp&nbsp  &ltCD&gt<br>
-				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp &lttitle&gtHide Your Heart&lt/title&gt<br>
+				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp &lttitle&gtHide your heart&lt/title&gt<br>
 				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp &ltartist&gtBonnie Tyler&lt/artist&gt<br>
-				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp &ltartwork&gthttps://upload.wikimedia.org/wikipedia/en/2/23/Hide_Your_Heart_Front_Cover.jpg&lt/artwork&gt<br>
 				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp &ltcountry&gtReino Unido&lt/country&gt<br>
 				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp &ltcompany&gtCBS Records&lt/company&gt<br>
-				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp &ltdescription&gtHide Your Heart (released under the title Notes from America in the United States, Canada and Brazil), is the seventh studio album by Welsh singer Bonnie Tyler. The album features the song "Hide Your Heart" written by Paul Stanley, Desmond Child and Holly Knight. This song was later covered three times in 1989 Ace Frehley's Trouble Walkin', Robin Beck's Trouble Or Nothin', and Molly Hatchet's Lightning Strikes Twice. And performed in 1989 by Paul Stanley with Kiss on their Hot in the Shade album.&lt/description&gt<br>
+				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp &ltdescription&gtHide Your Heart (released under the title Notes from America in the United States, Canada and Brazil), is the seventh studio album by Welsh singer Bonnie Tyler.&lt/description&gt<br>
 				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp &ltyear&gt1988&lt/year&gt &lt/CD&gt<br>
 				...
 			</p>
@@ -193,10 +191,10 @@ def relatorio():
 				O ficheiro que deu origem a todo o website começa pelo import das librarias necessárias na resolução de algoritmos.
 			</p>
 			<p class="codigo">
-				&nbsp&nbsp&nbsp from re import *<br>
+				&nbsp&nbsp&nbsp from re import * <br>
 				&nbsp&nbsp&nbsp import jinja2 as j2<br>
 				&nbsp&nbsp&nbsp import lxml<br>
-				&nbsp&nbsp&nbsp from bs4 import BeautifulSoup as bs<br>
+				&nbsp&nbsp&nbsp from bs4 import BeautifulSoup as bs <br>
 				&nbsp&nbsp&nbsp import sys<br>
 				&nbsp&nbsp&nbsp import os<br>
 				&nbsp&nbsp&nbsp import webbrowser
@@ -218,42 +216,10 @@ def relatorio():
 				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp cd.append(i)<br>
 			</p>
 			<p class="conteudo_relatorio">
-				Esta função começa por abrir o ficheiro catalogotp1.xml sendo este atribuido à variável f, usando a função read() em f criamos uma <br>
-				uma variável d que contém todo o conteúdo de catalogotp1.xml com o formato XML.<br>
-				Usando a libraria bs4 criamos uma soup que contém o conteúdo em d no formato xml. Com o auxílio do ciclo for e da função find_all<br>
+				Esta função começa por abrir o ficheiro catalogotp1.xml sendo este atribuido á variável f, usando a função read() em f criamos uma <br>
+				em f criamos uma variavel d que contém todo o conteúdo de catalogotp1.xml com o formato XML.<br>
+				Usando a libraria bs4 criamos uma soup que contém o conteúdo em d no formato xml. Com o auxilio do ciclo for e da função find_all<br>
 				encontramos todas as tags CD e guardamos o seu conteúdo na lista cd.<br>
-			</p>
-			<h3>Função create_page(cd)</h3>
-			<p class="codigo">
-				&nbsp&nbsp&nbsp def index(cd):<br>
-				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp a = j2.Template("""<br>
-				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp &lt!DOCTYPE html&gt<br>
-				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp &lthtml&gt<br>
-				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp &lthead&gt<br>
-				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp &ltmeta charset="UTF-8"&gt<br>
-				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp &lttitle&gtCatálogo de CDs&lt/title&gt<br>
-				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp &lt/head&gt<br>
-				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp &ltbody&gt<br>
-				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp	&lth1&gtÍndice&lt/h1&gt<br>
-				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp		&ltul&gt<br>
-				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp			{% for el in title %}<br>
-				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp		&ltli&gt&lta href="{{el}}.html"&gt{{el}}&lt/a&gt&lt/li&gt<br>
-				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp			{% endfor %}<br>
-				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp	&lt/ul&gt<br>
-				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp &lt/body&gt<br>
-				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp &lt/html&gt<br>
-				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp """)<br>
-				&nbsp&nbsp&nbsp for i in cd:<br>
-				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp title.append(i.title.text)<br>
-				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp f_output = open('index.html', 'w') <br>
-				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp print(a.render({'title':title}), file=f_output)<br>
-				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp indice = 'file:///'+os.getcwd()+'/' + 'index.html'<br>
-				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp webbrowser.open_new_tab(indice)
-			</p>
-			<p class="conteudo_relatorio">
-				Esta função consiste em criar um índice de CDs. Recebe como argumento o conteúdo dos CDs obtidos em get_cd().<br>
-				Cria um template que vai dar origem ao ficheiro index.html, em que para cada CD usamos o título do mesmo para criar as hiperligações necessárias.<br>
-				Por fim implementámos uma função de abertura do index.html automática após correr o programa.
 			</p>
 			<h3>Função index(cd)</h3>
 			<p class="codigo">
@@ -283,9 +249,9 @@ def relatorio():
 				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp webbrowser.open_new_tab(indice)
 			</p>
 			<p class="conteudo_relatorio">
-				Esta função consiste em criar um índice de CDs. Recebe como argumento o conteúdo dos CDs obtidos em get_cd().<br>
-				Cria um template que vai dar origem ao ficheiro index.html, em que para cada CD usamos o título do mesmo para criar as hiperligações necessárias.<br>
-				Por fim implementámos uma função de abertura do index.html automática após correr o programa.
+				Esta função consiste em criar um indice de CDs. Recebe como argumento o conteúdo dos CDs obtidos em get_cd().<br>
+				Cria um template que vai dar origem ao ficheiro index.html, em que para cada CD usamos o titulo do mesmo para criar as hiperligações necessárias.<br>
+				Por fim implementamos uma função de abertura do index.html automática após correr o programa.
 			</p>
 			<h3>Função main()</h3>
 			<p class="codigo">
@@ -309,7 +275,8 @@ def relatorio():
 	f_out = open('relatorio.html', 'w', encoding='utf-8')
 	print(k.render(), file=f_out)
 
-	st = j2.Template('''    body {
+	st = j2.Template('''    
+                                        body {
 										background-color:lightgrey;
 										width:100%;
 										max-width:1920px;}
@@ -319,7 +286,7 @@ def relatorio():
 					align-self: center;
 					padding-left: 12.5%;}
 
-				table {width:100%}
+                                        table {width:100%}
 
 								div.hiper {
 										border: 1px solid black;
@@ -332,10 +299,11 @@ def relatorio():
 										color: black;
 										border-radius: 15px;
 								}
-							div.art {
-							text-align:center;
+					div.art {
+					text-align:center;
+					
+					}
 
-							}
 								th {
 								width:33%;
 								}
@@ -386,7 +354,8 @@ def relatorio():
 
 				p.conteudo_relatorio {
 					font-size: 18px;
-					color:black;}
+					color:black;
+					text-align:center;}
 
 				p.codigo {
 					font-size: 18px;
