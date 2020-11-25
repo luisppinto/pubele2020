41128958ce40f6f1d3423b74cf9e427761b9a29a
luisppinto <72820660+luisppinto@users.noreply.github.com>
2020-11-25 15:24:11 +0000

    1

--- tp/tp1/v6/v604.py
+++ tp/tp1/v6/v604.py
@@ -11,7 +11,7 @@ cd=[] # Lista vazia (a preencher por get_cd()) de todos os cds e respetivos cont
 title=[]
 
 def get_codigo():
-	with open('v603.py', encoding='utf-8') as original, open('codigo.txt', "w", encoding='utf-8') as target:
+	with open('v604.py', encoding='utf-8') as original, open('codigo.txt', "w", encoding='utf-8') as target:
 		target.writelines(original.readlines())
 
 def get_cd():
@@ -28,14 +28,31 @@ def create_page(cd):
 	<head>
 			<title>{{title}}</title
 		<meta charset="UTF-8"/>
+		<link rel="stylesheet" href="stylesheet.css">
 	</head>
 	<body>
+	<div class="header">
+				<h1>Cátalogo de CDs</h1>
+		</div>
+		<div class="nav">
+			<table class="nav-tab">
+				<tr>
+						<th><a href="index.html"><div class="hed">Lista de Titulos</div></a></th>
+						<th><a href="relatorio.html"><div class="hed">Relatório</div></a></th>
+                        <th><a href="codigo.txt"><div class="hed">Código</a></div></th>
+                </tr>
+                        </table>
+		</div>
+
+		<div class="main_content">
+
 			<h1>{{newdic.title}}</h1>
 			<h2>{{newdic.artist}}</h2>
 			<p><b>Ano: </b>{{newdic.year}} <b>País: </b>{{newdic.country}} <b>Produtora: </b>{{newdic.company}} </p>
 			<h2> Descrição </h2>
 			<p>{{newdic.description}}</p>
 			<a href="index.html">Voltar ao índice</a>
+			</div>
 	</body>
 </html>
 """)
@@ -74,7 +91,7 @@ def index(cd):
                 <form name="search" action="~/query_scorer.py" method="get">
                         <input type="text" name="searchbox">
                         <input type="submit" value="Search">
-                </form> 
+                </form>
 	<html>
 		<head>
 			<title>Catálogo de CDs</title>
@@ -83,24 +100,24 @@ def index(cd):
 		</head>
 		<body>
 		<div class="header">
-				<h1>Cátalogo de CDs</h1>	
-		</div>	
+				<h1>Cátalogo de CDs</h1>
+		</div>
 		<div class="nav">
 			<table class="nav-tab">
 				<tr>
-					<th><a href="index.html">Lista de Titulos</a></th>
-					<th><a href="relatorio.html">Relatório</a></th>
-                                        <th><a href="codigo.txt">Código</a></th>
-                                </tr>
+						<th><a href="index.html"><div class="hed">Lista de Titulos</div></a></th>
+						<th><a href="relatorio.html"><div class="hed">Relatório</div></a></th>
+                        <th><a href="codigo.txt"><div class="hed">Código</a></div></th>
+                </tr>
                         </table>
 		</div>
-		
+
 		<div class="main_content">
 			<table>
                                 {% for el in title %}
 				<tr>
                                         <th><a href="{{el}}.html"><div class="hiper">{{el}}</div></a></th>
-				
+
 				</tr>
 				{% endfor %}
 			</table>
@@ -126,29 +143,29 @@ def relatorio():
 			<title>Relatório</title>
 			<link rel="stylesheet" href="stylesheet.css">
 		</head>
-		<body> 
+		<body>
 		<div class="header">
-				<h1>Cátalogo de CDs</h1>	
+				<h1>Cátalogo de CDs</h1>
 		</div>
 
 		<div class="nav">
 			<table>
 				<tr>
-					<th><a href="index.html">Lista de Titulos</a></th>
-					<th><a href="relatorio.html">Relatório</a></th>
-                                        <th><a href="codigo.txt">Código</a></th>
+					<th><a href="index.html"><div class="hed">Lista de Titulos</div></a></th>
+						<th><a href="relatorio.html"><div class="hed">Relatório</div></a></th>
+                                        <th><a href="codigo.txt"><div class="hed">Código</a></div></th>
                                 </tr>
 			</table>
 		</div>
-		<div class="main_content">	
+		<div class="main_content">
 			<h1>Relatório</h1>
 			<h2> Ficheiro Catálogotp1.xml </h2>
-			 <p class="conteudo_relatorio"> 
+			 <p class="conteudo_relatorio">
 			 	O ficheiro Catálogotp1.xml contém um conjunto de dados relativos a CDs de Música.<br>
 				Estes CDs contém um titulo, de caracter obrigatório, contém ainda um artista, um país, uma produtora, uma descrição e um ano.<br>
-				Abaixo podemos ver um excerto de código em XML: 
+				Abaixo podemos ver um excerto de código em XML:
 			</p>
-			<p class="codigo"> 
+			<p class="codigo">
 				&ltCATALOG&gt<br>
 				&nbsp&nbsp&nbsp  &ltCD&gt<br>
 				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp &lttitle&gtA Vida Que Eu Escolhi&lt/title&gt<br>
@@ -164,30 +181,30 @@ def relatorio():
 				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp &ltcompany&gtCBS Records&lt/company&gt<br>
 				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp &ltdescription&gtHide Your Heart (released under the title Notes from America in the United States, Canada and Brazil), is the seventh studio album by Welsh singer Bonnie Tyler.&lt/description&gt<br>
 				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp &ltyear&gt1988&lt/year&gt &lt/CD&gt<br>
-				...			
+				...
 			</p>
 			<h2> Código.py </h2>
-			<p class="conteudo_relatorio"> 
+			<p class="conteudo_relatorio">
 				O ficheiro que deu origem a todo o website começa pelo import das librarias necessárias na resolução de algoritmos.
 			</p>
-			<p class="codigo"> 
-				&nbsp&nbsp&nbsp from re import * <br>                          
-				&nbsp&nbsp&nbsp import jinja2 as j2<br>                    
-				&nbsp&nbsp&nbsp import lxml<br>                         
-				&nbsp&nbsp&nbsp from bs4 import BeautifulSoup as bs <br>    
-				&nbsp&nbsp&nbsp import sys<br>                 
-				&nbsp&nbsp&nbsp import os<br>                
-				&nbsp&nbsp&nbsp import webbrowser               
-			</p>           
-			<p class="conteudo_relatorio"> 
+			<p class="codigo">
+				&nbsp&nbsp&nbsp from re import * <br>
+				&nbsp&nbsp&nbsp import jinja2 as j2<br>
+				&nbsp&nbsp&nbsp import lxml<br>
+				&nbsp&nbsp&nbsp from bs4 import BeautifulSoup as bs <br>
+				&nbsp&nbsp&nbsp import sys<br>
+				&nbsp&nbsp&nbsp import os<br>
+				&nbsp&nbsp&nbsp import webbrowser
+			</p>
+			<p class="conteudo_relatorio">
 				Seguiu-se a inicialização das variáveis globais:
 			</p>
-			<p class="codigo"> 
+			<p class="codigo">
 				&nbsp&nbsp&nbsp cd=[] <br>
 				&nbsp&nbsp&nbsp title=[] <br>
 			</p>
 			<h3>Função get_cd()</h3>
-			<p class="codigo"> 
+			<p class="codigo">
 				&nbsp&nbsp&nbsp def get_cd():<br>
 				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp with open('catalogotp1.xml') as f:<br>
 				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp d=f.read()<br>
@@ -195,14 +212,14 @@ def relatorio():
 				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp for i in ad.find_all("CD"):<br>
 				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp cd.append(i)<br>
 			</p>
-			<p class="conteudo_relatorio">  
+			<p class="conteudo_relatorio">
 				Esta função começa por abrir o ficheiro catalogotp1.xml sendo este atribuido á variável f, usando a função read() em f criamos uma <br>
 				em f criamos uma variavel d que contém todo o conteúdo de catalogotp1.xml com o formato XML.<br>
 				Usando a libraria bs4 criamos uma soup que contém o conteúdo em d no formato xml. Com o auxilio do ciclo for e da função find_all<br>
 				encontramos todas as tags CD e guardamos o seu conteúdo na lista cd.<br>
 			</p>
 			<h3>Função index(cd)</h3>
-			<p class="codigo"> 
+			<p class="codigo">
 				&nbsp&nbsp&nbsp def index(cd):<br>
 				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp a = j2.Template("""<br>
 				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp &lt!DOCTYPE html&gt<br>
@@ -228,13 +245,13 @@ def relatorio():
 				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp indice = 'file:///'+os.getcwd()+'/' + 'index.html'<br>
 				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp webbrowser.open_new_tab(indice)
 			</p>
-			<p class="conteudo_relatorio"> 
+			<p class="conteudo_relatorio">
 				Esta função consiste em criar um indice de CDs. Recebe como argumento o conteúdo dos CDs obtidos em get_cd().<br>
 				Cria um template que vai dar origem ao ficheiro index.html, em que para cada CD usamos o titulo do mesmo para criar as hiperligações necessárias.<br>
-				Por fim implementamos uma função de abertura do index.html automática após correr o programa. 
+				Por fim implementamos uma função de abertura do index.html automática após correr o programa.
 			</p>
 			<h3>Função main()</h3>
-			<p class="codigo"> 
+			<p class="codigo">
 				&nbsp&nbsp&nbsp def main():<br>
 				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp	get_cd()<br>
 				&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp	create_page(cd)<br>
@@ -242,13 +259,13 @@ def relatorio():
 				<br>
 				&nbsp&nbsp&nbsp main()
 			</p>
-			<p class="conteudo_relatorio"> 
-				Esta função basicamente corre todos as funções nele contidas. 
+			<p class="conteudo_relatorio">
+				Esta função basicamente corre todos as funções nele contidas.
 			</p>
 			<br>
 			<a class="index" href="index.html">Voltar ao índice</a>
-		
-		</div>	
+
+		</div>
 		</body>
 	</html>
 ''')
@@ -266,8 +283,8 @@ def relatorio():
 					padding-left: 12.5%;}
 
 				table {width:100%}
-					                       
-                                div.hiper {                                        
+
+                                div.hiper {
                                         border: 1px solid black;
                                         background-color:grey;
                                         padding-top: 15px;
@@ -278,12 +295,21 @@ def relatorio():
                                         color: black;
                                         border-radius: 15px;
                                 }
-                                
+
                                 th {
                                 width:33%;
                                 }
+                                div.hed {border: 1px solid black;
+                                        background-color:grey;
+                                        padding-top: 15px;
+                                        padding-bottom: 15px;
+                                        text-align:center;
+                                        margin-top:5px;
+                                        font-size: 20px;
+                                        color: black;
+                                        border-radius: 15px;
+                                }
 
-				
 
 				div.main_content {
 					width:75%;
@@ -291,9 +317,9 @@ def relatorio():
 					align-self: center;
 					padding-left: 12.5%;}
 
-				h1 { 
+				h1 {
 					font-size: 40px;
-					color: black; 
+					color: black;
 					text-align:center;
 					height:72px;
 					padding-top:16px;
@@ -302,7 +328,7 @@ def relatorio():
 
 				h2 {
 					font-size: 30px;
-					color: black; 
+					color: black;
 					text-align:center;
 					height:72px;
 					padding-top:18px;
@@ -311,7 +337,7 @@ def relatorio():
 
 				h3 {
 					font-size: 24px;
-					color: black; 
+					color: black;
 					text-align:center;
 					height:72px;
 					padding-top:18px;
@@ -334,29 +360,27 @@ def relatorio():
 def searchbar():
 
         form = cgi.FieldStorage()
-
         searchterm = form.getvalue('searchbox')
-
         return searchterm
 
-def search():
+def search(searchterm):
 
         if searchterm != 0:
-
                 with open('catalogotp1.xml') as f:
-
                         d=f.read()
                         ad=bs(d,"xml")
                         results = ad.find_all(string=re.compile('.*{0}.*'.format(search)), recursive=True)
 
                         print('Foi encontrada a palavra "{0}" {1} vezes\n'.format(search, len(results)))
-	
+
 def main():
 	get_codigo()
 	get_cd()
 	create_page(cd)
 	index(cd)
 	relatorio()
-	
+	searchbar()
+	search(searchterm)
+
 	#print(cd) # Ver lista com cds e respetivos conteúdos
 main()
