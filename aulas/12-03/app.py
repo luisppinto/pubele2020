9070be0a21b4145e5aa2f8a3c04dc1025b6c4015
luisppinto <72820660+luisppinto@users.noreply.github.com>
2020-12-03 22:41:25 +0000

    aula 12/03

--- aulas/12-03/app.py
+++ aulas/12-03/app.py
@@ -1,59 +1,41 @@
 from flask import Flask, render_template
-import jinja2 as j2
 
 import random
 
+from bd import proverbios, pessoas
+
 app = Flask(__name__)
 
-proverbios = [
-'Mais vale tarde do que nunca.',
-'Mês de abril, arroz de caril.',
-'Quem vai à guerra dá e leva.'
-]
-
-proverbios = [
-{
-'id':0,
-'texto':'Mais vale tarde do que nunca.',
-'significado':'Atrasei-me, mas ao menos cheguei.'
-},
-{
-'id':1,
-'texto':'Mês de abril, arroz de caril.',
-'significado':'Arroz de caril é bom em abril.'
-},
-{
-'id':2,
-'texto':'Quem vai à guerra dá e leva.',
-'significado':'...'
-}
-]
-
-pessoas = [
-'Pedro',
-'Beatriz',
-'Paulo'
-]
-
-# OS TEMPLATES QUE AQUI ESTAVAM PASSARAM PARA A PASTA TEMPLATES
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
+
 
 @app.route('/proverbios')
 def proverbios_view():
-	return render_template('proverbios_view.html', title='Provérbios', proverbios=proverbios)
+    return render_template('proverbios_view.html', title='Proverbios', proverbios=proverbios)
 
-@app.route('/proverbios/proverbio/<id>')
-def proverbio_view(id):
-	return render_template('proverbio_view.html', p = proverbios[int(id)])
+
+@app.route('/proverbios/proverbio/<id_>')
+def proverbio_view(id_):
+    return render_template('proverbio_view.html', p = proverbios[int(id_)])
+
 
 @app.route('/proverbios/semana')
 def semana():
-	p = random.choice(proverbios)
-	return render_template('proverbio_semana.html', title='Provérbio da semana', proverbio=p)
+    p = random.choice(proverbios)
+    return render_template('proverbio_semana_view.html', proverbio=p)
+
 
 @app.route('/pessoas')
-def pessoas():
-	return render_template('pessoas_view.html', title='Pessoas', pessoas=pessoas)
+def pessoas_view():
+    return render_template('pessoas_view.html', title='Pessoas', pessoas=pessoas)
+
+
+
+
+
+
+
+
