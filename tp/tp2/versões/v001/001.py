419817de4f78fd317cece3c2d741dfbe68e3f0cc
luisppinto <72820660+luisppinto@users.noreply.github.com>
2021-01-21 21:15:30 +0000

    001 update

--- tp/tp2/versões/v001/001.py
+++ tp/tp2/versões/v001/001.py
@@ -1,61 +1,114 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import json
 import requests
-import random
-import shelve
-from bd import relatorios
+from re import *
+from povoamento import relatorios, pessoas
+import db
 
 app = Flask(__name__)
 
-s = shelve.open('relatorios.db')
+for relatorio in relatorios:
+    db.insert(relatorio)
+
+for pessoa in pessoas:
+    db.insert_p(pessoa)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    ps = db.find_all()
+    pessoas = db.find_all_p()
+    h = []
+    for titulo in ps:
+        relatorio = db.find_one(titulo)
+        for tag in relatorio['hashtags']:
+            if tag not in h:
+                h.append(tag)
+    return render_template('index.html', titles=ps, hashtags=h, pessoas=pessoas)
 
-@app.route('/api/relatorios')
-def api_relatorios():
-    ps = list(s.keys())
-    return json.dumps(ps)
+@app.route('/relatorios', methods=['GET'])
+def get_relatorios():
+    res = requests.get('http://localhost:5000/api/relatorios')
+    ps = json.loads(res.content)
+    return render_template('relatorios_view.html', title='Relatórios', relatorios=ps)
 
-@app.route('/relatorios')
-def relatorios_view():
+@app.route('/relatorios', methods=['POST'])
+def post_relatorio():
+    data = dict(request.form)
+    requests.post('http://localhost:5000/api/relatorios', data=data)
+    return redirect('http://localhost:5000/relatorios')
 
+@app.route('/relatorios/<titulo>', methods=['GET'])
+def get_relatorio(titulo):
+    res = requests.get('http://localhost:5000/api/relatorios/' + titulo)
+    p = json.loads(res.content)
+
+    return render_template('relatorio_view.html', p=p)
+
+@app.route('/relatorios/procura')
+def procura_view():
+    return render_template('procura_view.html')
+
+@app.route('/pessoas/<nome>', methods=['GET'])
+def get_pessoa(nome):
+    res = requests.get('http://localhost:5000/api/pessoas/' + nome)
+    p = json.loads(res.content)
+    return render_template('pessoa_view.html', p=p)
+
+@app.route('/relatorios/resultado', methods=['POST'])
+def relatorios_resultado():
+    pesquisa = request.form.get('procura').replace('#','')
     res = requests.get('http://localhost:5000/api/relatorios')
     ps = json.loads(res.content)
 
-    return render_template('relatorios_view.html', title='Proverbios', relatorios=ps)
+    l = []
+    t = []
+    for titulo in ps:
+        relatorio = db.find_one(titulo)
+        text = relatorio['description']
+        res = findall(rf"(?i)\b{pesquisa}\b",str(text))
+        if res:
+            if titulo not in l:
+                l.append(titulo)
+                pos = text.index(res[0])
+                if (len(text) >= 500):
+                    t.append(text[pos:pos+len(res[0])+500].replace(res[0],'*'+res[0]+'*'))
+                else:
+                    t.append(text.replace(res[0],'*'+res[0]+'*'))
+    return render_template('resultados_pesquisa_view.html', title=rf'Relatórios que contêm o padrão"{pesquisa}"', relatorios=l, i=)
 
-@app.route('/relatorios/novo', methods=['POST'])
-def processamento_ficheiro():
-    
+# api para relatorios
 
-def relatorios_novo():
+@app.route('/api/relatorios', methods=['GET'])
+def api_get_relatorios():
+    ps = db.find_all()
+    return json.dumps(ps)
 
-    title = request.form.get('title')
-    signi = request.form.get('autores')
+@app.route('/api/relatorios', methods=['POST'])
+def api_post_relatorio():
+    data = dict(request.form)
 
-    s[title] = signi
-    s.sync()
-    ps = list(s.keys())
+    h = data['hashtags'].split(' ')
+    l = data['references'].split(' ')
 
-    return render_template('relatorios_view.html', title='Relatorios', relatorios=ps)
+    data['references'] = l
+    data['hashtags'] = h
+    data['authors'] = [{'name': 'Bruno Rebelo Lopes', 'number': '57768'},{'name':'Morgana Sacramento Ferreira','number':'93779'},{'name':'Luís Pedro da Silva Pinto','number':'83016'}]
 
+    db.insert(data)
 
+    return json.dumps(db.find_all())
 
-@app.route('/relatorios/proverbio/<id_>')
-def proverbio_view(id_):
-    significado = s[id_]
-    return render_template('proverbio_view.html', p = {'title': id_, 'significado': significado})
+@app.route('/api/relatorios/<titulo>', methods=['GET'])
+def api_get_relatorio(titulo):
+    p = db.find_one(titulo)
+    return json.dumps(p)
 
-
-@app.route('/relatorios/semana')
-def semana():
-
-    p = random.choice(s.keys())
-    return render_template('proverbio_semana_view.html', proverbio=p)
-
-
-@app.route('/pessoas')
-def pessoas_view():
-    return render_template('pessoas_view.html', title='Pessoas', pessoas=pessoas)
+# api para autores
+@app.route('/api/pessoas', methods=['GET'])
+def api_get_pessoas():
+    ps = db.find_all_p()
+    return json.dumps(ps)
+@app.route('/api/pessoas/<nome>', methods=['GET'])
+def api_get_pessoas(nome):
+    p = db.find_one_p(nome)
+    return json.dumps(p)
