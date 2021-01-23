498a7693ad6e46d665810bd536fde8e4df6ac59a
luisppinto <72820660+luisppinto@users.noreply.github.com>
2021-01-23 23:50:34 +0000

    .

--- tp/tp2/versões/v003/v003.py
+++ tp/tp2/versões/v003/v003.py
@@ -1,16 +1,13 @@
 from flask import Flask, render_template, request, redirect
-import shelve
 import json
 import requests
-from db_cds import *
+from db_cds import *  
 
 app = Flask(__name__) # required
 
-s = shelve.open('cds.db')
-
-	# INDEX
+	# INDEX 
 	# Lista de CDs
-		# FRONT END
+		# FRONT END 
 @app.route('/', methods=['GET'])
 def index_view():
 	res = requests.get('http://localhost:5000/api/cds')
@@ -25,48 +22,66 @@ def api_get_cds():
 	return json.dumps(ps)
 
 	# Informações adicionais
-	# Lista autores
+	# Lista autores 
 		# FRONT END
 @app.route('/autores', methods=['GET'])
 def info_ad_view():
 	res = requests.get('http://localhost:5000/api/autores')
 	ss = json.loads(res.content)
 	return render_template('info_ad_view.html', autores=ss)
-
+	
 		# BACKEND
 @app.route('/api/autores', methods=['GET'])
 def api_get_autores():
 	ss = autores
 	return json.dumps(ss)
 
-	# ADICIONAR CD
-@app.route('/api/novocd', methods=['POST'])
+	# Ver CD
+
+@app.route('/cds/<cd>', methods=['GET'])
+def get_cd_view(cd):
+
+	res = requests.get('http://localhost:5000/api/cds/'+ cd['title'])
+	p = json.loads(res.content)
+
+	return render_template('cd_view.html', p=p)
+
+@app.route('/api/cds/<cd>', methods=['GET'])
+def api_get_cd(cd):
+
+	p = find_one(cd)
+	return json.dumps(p)
+
+@app.route('/api/cds/novocd', methods=['POST'])
 def api_cd_novo():
-
-	data = dict(request.form)
-	insert(data)
-	return json.dumps(data)
-
-def insert(cd):
+	# 1º abrimos o shelve
 	with shelve.open('cds.db', writeback=True) as s:
-		s[cd['title']] = cd['title']
-		s[cd['artist']] = cd['artist']
-		s[cd['year']] = cd['year']
-		s[cd['country']] = cd['country']
-		s[cd['company']] = cd['company']
-		s[cd['artwork']] = cd['artwork']
-		s[cd['id']] = cd['id']
-		return s
+	# 2º passamos do html para variavel
+		id = request.form.get('id')
+		title = request.form.get('title')
+		artist = request.form.get('artist')
+		artwork = request.form.get('artwork')
+		country = request.form.get('country')
+		company = request.form.get('company')
+		description = request.form.get('description')
+		year = request.form.get('year')
 
-@app.route('/cds/novo', methods=['POST'])
-def cds_novo():
+	# 3º Indicamos qual a posição de cada conteudo no shelve
+		s[cd['id']] = id
+		s[cd['title']] = title
+		s[cd['artist']] = artist
+		s[cd['artwork']] = artwork
+		s[cd['country']] = country
+		s[cd['company']] = company
+		s[cd['description']] = description
+		s[cd['year']] = year
 
-	title = request.form.get('title')
-	artist = request.form.get('artist')
-	artwork = request.form.get('artwork')
-	country = request.form.get('country')
-	company = request.form.get('company')
-	description = request.form.get('description')
-	year = request.form.get('year')
+	# 4º Atualizamos o conteudo do shelve
+		s.sync()
+		ps = list(s.keys())
 
-	return render_template('index.html', title='CDs', cds=ps)
+	return render_template('add_cd_view.html', title='CDs', cds=ps)
+
+@app.route('/cds/novocd')
+def novo_cd():
+	return render_template('add_cd_view.html')
