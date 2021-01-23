814204cc58128ce0ed6a72c525871d12a335738c
luisppinto <72820660+luisppinto@users.noreply.github.com>
2021-01-23 21:41:43 +0000

    HELLO MUNDO

--- tp/tp2/versões/v003/v003.py
+++ tp/tp2/versões/v003/v003.py
@@ -6,21 +6,32 @@ from db_cds import *
 app = Flask(__name__) # required
 
 	# INDEX 
-		# Lista de CDs
-# FRONT END 
+	# Lista de CDs
+		# FRONT END 
 @app.route('/', methods=['GET'])
 def index_view():
-
 	res = requests.get('http://localhost:5000/api/cds')
 	ps = json.loads(res.content)
-
 	return render_template('index.html', cds=ps)
 
-# BACKEND
 
+		# BACKEND
 @app.route('/api/cds', methods=['GET'])
-
 def api_get_cds():
 	ps = cds
 	return json.dumps(ps)
 
+	# Informações adicionais
+	# Lista autores 
+		# FRONT END
+@app.route('/autores', methods=['GET'])
+def info_ad_view():
+	res = requests.get('http://localhost:5000/api/autores')
+	ss = json.loads(res.content)
+	return render_template('info_ad_view.html', autores=ss)
+	
+		# BACKEND
+@app.route('/api/autores', methods=['GET'])
+def api_get_autores():
+	ss = autores
+	return json.dumps(ss)
