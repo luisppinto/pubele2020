9201a366c0d1fa0af08b2cf81305bb0bd3f96f75
luisppinto <lpsp@hotmail.com>
2021-01-24 15:52:26 +0000

    .

--- tp/tp2/versões/v0043/v003.py
+++ tp/tp2/versões/v0043/v003.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template, request, redirect
 import json
 import requests
-from db_cds import *  
+from db_cds import *
 
 app = Flask(__name__) # required
 
-	# INDEX 
+	# INDEX
 	# Lista de CDs
-		# FRONT END 
+		# FRONT END
 @app.route('/', methods=['GET'])
 def index_view():
 	res = requests.get('http://localhost:5000/api/cds')
@@ -22,14 +22,14 @@ def api_get_cds():
 	return json.dumps(ps)
 
 	# Informações adicionais
-	# Lista autores 
+	# Lista autores
 		# FRONT END
 @app.route('/autores', methods=['GET'])
 def info_ad_view():
 	res = requests.get('http://localhost:5000/api/autores')
 	ss = json.loads(res.content)
 	return render_template('info_ad_view.html', autores=ss)
-	
+
 		# BACKEND
 @app.route('/api/autores', methods=['GET'])
 def api_get_autores():
