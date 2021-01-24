b1bdfccac956e699fd884e99ee64a15665911f39
luisppinto <72820660+luisppinto@users.noreply.github.com>
2021-01-24 15:52:05 +0000

    003

--- tp/tp2/versões/v0043/v003.py
+++ tp/tp2/versões/v0043/v003.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template, request, redirect
 import json
 import requests
-from db_cds import *  
+import db_cds
 
 app = Flask(__name__) # required
 
-	# INDEX 
+	# INDEX
 	# Lista de CDs
-		# FRONT END 
+		# FRONT END
 @app.route('/', methods=['GET'])
 def index_view():
 	res = requests.get('http://localhost:5000/api/cds')
@@ -22,14 +22,14 @@ def api_get_cds():
 	return json.dumps(ps)
 
 	# Informações adicionais
-	# Lista autores 
+	# Lista autores
 		# FRONT END
 @app.route('/autores', methods=['GET'])
 def info_ad_view():
 	res = requests.get('http://localhost:5000/api/autores')
 	ss = json.loads(res.content)
 	return render_template('info_ad_view.html', autores=ss)
-	
+
 		# BACKEND
 @app.route('/api/autores', methods=['GET'])
 def api_get_autores():
@@ -39,7 +39,7 @@ def api_get_autores():
 def novo_cd_view():
     return render_template('add_cd_view.html')
 
-@app.route('/api/cds/novocd', methods=['GET'])
+@app.route('/api/cds/novocd', methods=['POST'])
 def api_novo_cd():
 	title = request.form.get('title')
 	artist = request.form.get('artist')
