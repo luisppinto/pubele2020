1f93d12c46d03e6877aadebcd3c3965a0f7abb39
luisppinto <72820660+luisppinto@users.noreply.github.com>
2021-01-25 23:15:47 +0000

    .

--- tp/tp2/versões/v006/auto_server.py
+++ tp/tp2/versões/v006/auto_server.py
@@ -1,5 +1,4 @@
 import os
 def opencmd():
-  os.system('cmd /k "env\Scripts\activate & set FLASK_APP=v006.py & flask run"')
+  os.system('terminal /k "env/Scripts/activate & set FLASK_APP=v006.py & flask run"')
 opencmd()
-  
