445118cc6d31b1092217538b924d747eb6e4a1b4
luisppinto <72820660+luisppinto@users.noreply.github.com>
2021-01-25 13:53:47 +0000

    6

---
+++ tp/tp2/versões/v006/dbcds2.py
@@ -0,0 +1,18 @@
+import shelve
+
+def find_all():
+    with shelve.open('cds.db') as s:
+        return list(s.keys())
+
+def find_one(title):
+    with shelve.open('cds.db') as s:
+        return s[title]
+
+def insert(cd):
+    with shelve.open('cds.db', writeback=True) as s:
+        s[cd['title']] = cd
+        return s
+
+def delete(title):
+	with shelve.open('cds.db', writeback=True) as s:
+		del s[title]
